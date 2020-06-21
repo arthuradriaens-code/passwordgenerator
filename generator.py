f27e6803634a843dd53f12ecc3015f544ac9df69
syphix99 <arthur99.adriaens@gmail.com>
2020-06-21 02:14:08 +0200

    It should now be working

--- generator.py
+++ generator.py
@@ -3,14 +3,15 @@ import pyperclip
 import random
 root = Tk()
 root.geometry("400x400")
-passstr = ""
-passlen = 0
+passstr = StringVar()
+passlen = IntVar()
+passlen.set(0)
 
 def generate():
 	#all char between 33 and 125 except 34 and 92
 	password = ""
-	for i in range(passlen):
-		password += random.choice(char(33),char(randomint(35,91)),char(randomint(93,125)))
+	for i in range(passlen.get()):
+		password += random.choice([chr(33),chr(random.randint(35,91)),chr(random.randint(93,125))])
 	passstr.set(password)
 
 def copytoclipboard():
@@ -24,4 +25,4 @@ Button(root, text="Generate Password", command=generate).pack(pady=7)
 Entry(root, textvariable=passstr).pack(pady=3)
 Button(root,text="Copy to clipboard",command=copytoclipboard).pack()
 
-root.mainloop() 	
+root.mainloop()
